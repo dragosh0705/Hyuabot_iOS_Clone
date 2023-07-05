2df5c9b1232e82919ce8ab037cdea1892fe6ecea
dragosh0705 <pingu0705@hanyang.ac.kr>
2023-07-06 02:38:33 +0900

    가로 테이블 뷰 구현 완료

--- Hyuabot_iOS_Clone/ViewController.swift
+++ Hyuabot_iOS_Clone/ViewController.swift
@@ -1,65 +1,66 @@
-
 import UIKit
 
-class ViewController: UIViewController {
-    
-    private var ShuttleBusLabel : UILabel = {
-        var MainLabel = UILabel()
-        MainLabel.text = "셔틀버스"
-        MainLabel.font = UIFont.boldSystemFont(ofSize: 30)
-        MainLabel.translatesAutoresizingMaskIntoConstraints = false
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    private var shuttleBusLabel: UILabel = {
+        var mainLabel = UILabel()
+        mainLabel.text = "셔틀버스"
+        mainLabel.font = UIFont.boldSystemFont(ofSize: 30)
+        mainLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        return MainLabel
+        return mainLabel
     }()
     
-    private var stack: UIStackView {
-        let stack = UIStackView()
-        stack.axis = .horizontal
-        stack.spacing = 8
-        stack.distribution = .fill
-        stack.alignment = .leading
-        stack.layoutMargins = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
-        stack.isLayoutMarginsRelativeArrangement = true
+    private var TopTableView: UITableView = {
+        var tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.isScrollEnabled = true
+        tableView.showsHorizontalScrollIndicator = true
+        tableView.showsVerticalScrollIndicator = false
+        tableView.transform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi / 2))
         
-        stack.layer.cornerRadius = 14.0
-        stack.layer.shadowColor = UIColor.black.cgColor
-        stack.layer.shadowOffset = CGSize(width: 0.0, height: 3.0)
-        stack.layer.shadowOpacity = 0.1
-        stack.layer.shadowRadius = 3.0
-
-        return stack
-    }
-
-
+        return tableView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.addSubview(ShuttleBusLabel)
-        self.view.addSubview(stack)
+        self.view.addSubview(shuttleBusLabel)
+        self.view.addSubview(TopTableView)
         
         NSLayoutConstraint.activate([
-            self.ShuttleBusLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30),
-            self.ShuttleBusLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 80)
+            shuttleBusLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30),
+            shuttleBusLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 80)
         ])
-        
-        // Add subviews to the stack view
-        for _ in 1...3 {
-            let subview = UIView()
-            subview.backgroundColor = .red
-            stack.addArrangedSubview(subview)
-            subview.widthAnchor.constraint(equalToConstant: 50).isActive = true
-            subview.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        }
         
         NSLayoutConstraint.activate([
-            self.stack.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30),
-            self.stack.topAnchor.constraint(equalTo: self.ShuttleBusLabel.bottomAnchor, constant: 16),
-            self.stack.trailingAnchor.constraint(lessThanOrEqualTo: self.view.trailingAnchor, constant: -30)
+            TopTableView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            TopTableView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 150),
+            TopTableView.widthAnchor.constraint(equalToConstant: 350),
+            TopTableView.heightAnchor.constraint(equalToConstant: 350) // 테이블 뷰의 높이 조정
         ])
+        
+        TopTableView.delegate = self
+        TopTableView.dataSource = self
     }
-
-
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        // 셀 설정
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 350 // 각 셀의 높이 설정
+    }
+    
+    
 }
-
